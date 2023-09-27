9c780ae7f42b1b4d72319a9f7cc682c62866fa4d
slcloe <gkstmf1403@naver.com>
2023-09-27 15:40:41 +0900

    0927 알고리즘

--- 코딩테스트/src/boj/boj11053.java
+++ 코딩테스트/src/boj/boj11053.java
@@ -1,29 +1,51 @@
 package boj;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
 public class boj11053 {
-    private static int n;
+    private static int[] dp;
     private static int[] arr;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        arr = new int[n];
-        for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
-        System.out.println(go(0));
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        arr = new int[N];
+        int[] LIS = new int[N]; // 각수를 끝에 세울수 있는 최장길이
+        dp = new int[N];
+        Arrays.fill(dp,-1);
+        for (int i = 0; i < N; i++) arr[i] = s.nextInt();
+
+        int max = 0;
+        for(int i=0; i<N; ++i) { // 기존 증가수열에 덧붙일 대상
+            LIS[i] = 1; // 일단 현재의 수만 수열에 넣었을때 길이로 초기화.
+            for(int j=0; j<i; ++j) { // 결국, 자신의 앞쪽에 세울수 있는(자신보다 작은) 애 중에 가장 긴 최장길이에 자신을 붙인다.
+                if(arr[j]<arr[i] && LIS[i]<LIS[j]+1) {  // 최대값 갱신
+                    LIS[i]=LIS[j]+1;
+                }
+            }// 결국, LIS[i]에는 자신을 끝으로 하는 최대값이 저장되어 있음
+
+            // 최장길이최대값 갱신
+            if(max<LIS[i]) max = LIS[i];
+        }
+
+//        System.out.println(max);
+        int res = 0;
+        for(int i = 0; i < N; i++){
+            res = Math.max(res, topdown(i));
+        }
+        System.out.println(res);
     }
 
-    // idx 위치에서 시작해서 만들 수 있는 가장 긴 증가수열
-    // arr[idx] < arr[idx+1] 이면 go(idx+1) + 1
-    // 그렇지 않으면 go(idx+1)
-    private static int go(int idx){
-        if(idx == n-1) return 1;
+    private static int topdown(int idx) {
+        if(idx == 0) return 1;
+        if(dp[idx] != -1) return dp[idx];
         int res = 1;
-        for(int i = idx+1; i < n; i++){
-            if(arr[idx] < arr[i]) {
-                res = Math.max(1,go(i) + 1);
+        for(int i = idx-1; i >= 0; i--){
+            if(arr[i] < arr[idx]){
+                res = Math.max(res, topdown(i) + 1);
             }
         }
+        dp[idx] = res;
         return res;
     }
 
--- 코딩테스트/src/boj/boj3055.java
+++ 코딩테스트/src/boj/boj3055.java
@@ -3,14 +3,14 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class boj3055 {
     private static int r,c,sx,sy;
     private static char[][] map;
     private static List<int[]> wlist;
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0,1,0,-1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -27,9 +27,45 @@ public class boj3055 {
                 }else if(cur == '*'){
                     wlist.add(new int[]{i, j});
                 }
-
+                map[i][j] = cur;
             }
         }
-
+        int res = go();
+        if(res == -1) System.out.println("KAKTUS");
+        else System.out.println(res);
+    }
+    private static int go(){
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        int[][] visited = new int[r][c];
+        for(int[] w: wlist){
+            int x = w[0]; int y = w[1];
+            q.add(new int[]{x,y,1,0});
+            visited[x][y] = 2;
+        }
+        q.add(new int[]{sx,sy,0,0});
+        visited[sx][sy] = 1;
+        while (!q.isEmpty()) {
+            int[] poll = q.poll();
+//            System.out.println(Arrays.toString(poll));
+            int move = poll[3];
+            if(map[poll[0]][poll[1]] == 'D') return move;
+            for (int k = 0; k < 4; k++) {
+                int nx = poll[0] + dx[k];
+                int ny = poll[1] + dy[k];
+                if(nx < 0 || nx >= r || ny < 0 || ny >= c || map[nx][ny] == 'X') continue;
+                if(poll[2] == 1){ // 물이라면
+                    if(visited[nx][ny] <= 1 && map[nx][ny] != 'D'){
+                        visited[nx][ny] = 2;
+                        q.add(new int[]{nx,ny,1,move+1});
+                    }
+                } else if(poll[2] == 0){
+                    if (visited[nx][ny] == 0){
+                        visited[nx][ny] = 1;
+                        q.add(new int[]{nx,ny,0,move+1});
+                    }
+                }
+            }
+        }
+        return -1;
     }
 }
